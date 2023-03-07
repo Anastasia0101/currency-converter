49ee833a9b8a323f96a84ca9feec095075c006ea
Anastasia Storozhenko <anastasiastorozhenko@Anastasias-MacBook-Air.local>
2023-03-07 10:16:59 +0200

    added UI select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './UI/input/input.component';
+import { SelectComponent } from './UI/select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
+    SelectComponent,
   ],
   imports: [
     BrowserModule
