1c0bbe37c66a22929737c2c1060870eddada0960
Anastasia Storozhenko <anastasiastorozhenko@Anastasias-MacBook-Air.local>
2023-03-08 13:22:32 +0200

    added converting currency with data from form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CurrencyEnum } from 'src/app/enums/CurrencyEnum';
+import { CurrenciesService } from 'src/app/services/currencies.service';
 
 @Component({
   selector: 'app-form',
@@ -19,5 +20,19 @@ export class FormComponent {
 
   constructor(
     private formBuilder: FormBuilder,
+    private currenciesService: CurrenciesService
   ) { }
+
+  onControlChanged(prefixInputedControl: string): void {
+    console.log('on change control')
+    const prefixWantedControl = prefixInputedControl === 'give' ? 'take' : 'give';
+
+    const inputedAmount = this.formGroup.get(`${prefixInputedControl}Amount`)!.value;
+    const wantedCurrencyCode = this.formGroup.get(`${prefixWantedControl}CurrencyCode`)!.value;
+    const inputedCurrencyCode = this.formGroup.get(`${prefixInputedControl}CurrencyCode`)!.value;
+    
+    this.currenciesService.convertCurrencies(wantedCurrencyCode, inputedCurrencyCode, inputedAmount).subscribe(data => {
+      this.formGroup.get(`${prefixWantedControl}Amount`)!.setValue(data.result);
+    });
+  }
 }
